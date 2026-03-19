6cd0ffd36c425343a047279a2fc55a45afbd60b4
mikshasa0207 <samikshajangid30@gmail.com>
2026-03-19 11:45:51 +0530

    fix JSX parsing issue in ESLint

--- client/src/App.test.jsx
+++ client/src/App.test.jsx
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("renders ShopSmart title", async () => {
-  render(<App />);
+  render(< App />);
 
   const heading = await screen.findByRole("heading", {
     name: /shop/i,
