42402fbed441e785217caaf1d1fd06f42406c041
mikshasa0207 <samikshajangid30@gmail.com>
2026-05-06 14:24:08 +0530

    done

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "shopsmart-tfstate-df62308f"
+    bucket  = "shopsmart-tfstate-samiksha-20260505"
     key     = "shopsmart/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
